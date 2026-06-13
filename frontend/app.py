49785a7101ba9c30bf6973bdefeeeeb2f995493d
Sakshi <sakshi.kawade@parag.com>
2026-06-13 18:29:40 +0530

    fix: update BASE_URL to use the production endpoint

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://ai-commit-messager.onrender.com/"
 
 st.set_page_config(
     page_title="AI Git Assistant",
